099644e2ef7b1919769b76ea1f92f31e942a2c7e
LeulBerhanu <leulberhanua@gmail.com>
2024-12-16 19:30:26 +0300

    add icons to Footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import mongodb from "/images/mongodb.svg";
+import express from "/images/express.png";
+import react from "/images/react.png";
+import node from "/images/node.png";
+import tanstack from "/images/tanstack.png";
+import Icon from "@/components/Icon";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="py-12">
+      <div className="flex gap-4 justify-center">
+        <Icon content="Tanstack">
+          <img
+            className="w-10 h-10 object-contain hover:-translate-y-1 transition duration-300 "
+            src={tanstack}
+            alt=""
+          />
+        </Icon>
+        <Icon content="Mongodb">
+          <img
+            className="w-10 h-10 object-contain hover:-translate-y-1 transition duration-300 "
+            src={mongodb}
+            alt=""
+          />
+        </Icon>
+        <Icon content="Express JS">
+          <img
+            className="w-10 h-10 object-contain hover:-translate-y-1 transition duration-300 "
+            src={express}
+            alt=""
+          />
+        </Icon>
+        <Icon content="React JS">
+          <img
+            className="w-10 h-10 object-contain hover:-translate-y-1 transition duration-300 "
+            src={react}
+            alt=""
+          />
+        </Icon>
+        <Icon content="Node JS">
+          <img
+            className="w-10 h-10 object-contain hover:-translate-y-1 transition duration-300 "
+            src={node}
+            alt=""
+          />
+        </Icon>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
