ecda0364b72645a469fed4dc4248401b13713b38
LeulBerhanu <leulberhanua@gmail.com>
2024-12-16 19:31:19 +0300

    refactor router

--- src/routing/routes.jsx
+++ src/routing/routes.jsx
@@ -1,10 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "@/Pages/HomePage";
+import { AddVehiclePage } from "@/Pages/Vehicles/AddVehiclePage";
+import { UpdateVehiclePage } from "@/Pages/Vehicles/UpdateVehiclePage";
+import ErrorPage from "@/Pages/ErrorPage";
+import MainLayout from "@/layouts/MainLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "add-vehicle",
+        element: <AddVehiclePage />,
+      },
+      {
+        path: "update-vehicle/:vehicleId",
+        element: <UpdateVehiclePage />,
+      },
+    ],
   },
 ]);
 
