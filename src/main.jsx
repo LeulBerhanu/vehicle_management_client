113db0c125994936635a3b65f1305cb0b391f0ca
LeulBerhanu <leulberhanua@gmail.com>
2024-12-16 19:31:59 +0300

    add query client provider and rm Main

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import Main from "./layouts/Main";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router";
+import "./index.css";
 import router from "./routing/routes";
+import MainLayout from "./layouts/MainLayout";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Main>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </Main>
+    </QueryClientProvider>
   </StrictMode>
 );
