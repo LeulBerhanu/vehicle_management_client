84841dfb6b63ba1db408debcaf54945908eb4da8
LeulBerhanu <leulberhanua@gmail.com>
2024-12-16 19:31:04 +0300

    create Main Layout

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./Footer";
+import { Toaster } from "@/components/ui/toaster";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  return (
+    <div className="flex flex-col h-dvh">
+      <Header />
+      <main className="container mx-auto flex-1">
+        <Outlet />
+      </main>
+      <Toaster />
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
